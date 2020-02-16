a3ce32b94f36fdc1f4cc7475577ece6001621cca
DiegoKiller98 <43404893+DiegoKiller98@users.noreply.github.com>
2020-02-16 17:18:46 +0100

    Initial commit

---
+++ GUI for AI/InputNode.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System;
+
+public class InputNode : BaseInputNode
+{
+
+    public InputNode()
+    {
+        windowTitle = "Input Node";
+    }
+
+    public override void DrawWindow()
+    {
+        base.DrawWindow();
+    }
+
+    public override void DrawCurves()
+    {
+        
+    }
+}
